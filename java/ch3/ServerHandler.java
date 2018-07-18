1414ee66c84e7a2c27a5409378f38d81aed40663
cheng <792702352@qq.com>
2018-07-18 14:32:39 +0800

    update netty server example

--- java/ch3/ServerHandler.java
+++ java/ch3/ServerHandler.java
@@ -3,8 +3,6 @@ package ch3;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * 服务器启动过程中打印对应的状态信息
  *
@@ -26,23 +24,5 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) {
         System.out.println("handlerAdded");
-    }
-
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
-
-        new Thread(()->{
-            // 耗时的操作
-            String result = loadFromDB();
-
-            ctx.channel().writeAndFlush(result);
-            ctx.executor().schedule(() -> {
-                // ...
-            }, 1, TimeUnit.SECONDS);
-        }).start();
-    }
-
-    private String loadFromDB() {
-        return "hello world";
     }
 }
