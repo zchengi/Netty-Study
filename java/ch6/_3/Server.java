d149be306522042d595cca1b29de752b0101e92c
cheng <792702352@qq.com>
2018-07-20 15:44:27 +0800

    添加 ChannelHandler

---
+++ java/ch6/_3/Server.java
@@ -0,0 +1,45 @@
+package ch6._3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.AttributeKey;
+
+/**
+ * 添加 ChannelHandler
+ *
+ * @author cheng
+ *         2018/7/20 14:47
+ */
+public class Server {
+    public static void main(String[] args) throws InterruptedException {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.TCP_NODELAY, true)
+                    .childAttr(AttributeKey.newInstance("childAttr"), "childAttrValue")
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter());
+                            ch.pipeline().addLast(new ChannelOutboundHandlerAdapter());
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
+
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
